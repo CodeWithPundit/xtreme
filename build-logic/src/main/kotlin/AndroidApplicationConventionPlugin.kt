69546b7acae8362ef5f0ed2b268e0de70bc4fcb9
Code With Pundit <codewithpundit@gmail.com>
2026-03-11 11:34:34 -0400

    Create AndroidApplicationConventionPlugin.kt

---
+++ build-logic/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -0,0 +1,79 @@
+import com.android.build.api.dsl.ApplicationExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+class AndroidApplicationConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("com.android.application")
+                apply("org.jetbrains.kotlin.android")
+                apply("org.jetbrains.kotlin.kapt")
+            }
+
+            extensions.configure<ApplicationExtension> {
+                compileSdk = 34
+
+                defaultConfig {
+                    minSdk = 24
+                    targetSdk = 34
+
+                    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+                }
+
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_17
+                    targetCompatibility = JavaVersion.VERSION_17
+                }
+
+                buildTypes {
+                    release {
+                        isMinifyEnabled = true
+                        isShrinkResources = true
+                        proguardFiles(
+                            getDefaultProguardFile("proguard-android-optimize.txt"),
+                            "proguard-rules.pro"
+                        )
+                    }
+                    
+                    debug {
+                        isMinifyEnabled = false
+                        applicationIdSuffix = ".debug"
+                    }
+                }
+
+                buildFeatures {
+                    compose = true
+                    buildConfig = true
+                }
+
+                packaging {
+                    resources {
+                        excludes += "/META-INF/{AL2.0,LGPL2.1}"
+                    }
+                }
+            }
+
+            tasks.withType<KotlinCompile>().configureEach {
+                kotlinOptions {
+                    jvmTarget = "17"
+                }
+            }
+
+            dependencies {
+                add("implementation", platform(libs.findLibrary("androidx-compose-bom").get()))
+                add("implementation", libs.findLibrary("androidx-compose-ui").get())
+                add("implementation", libs.findLibrary("androidx-compose-ui-graphics").get())
+                add("implementation", libs.findLibrary("androidx-compose-material3").get())
+                add("implementation", libs.findLibrary("androidx-compose-material3-window-size").get())
+                add("implementation", libs.findLibrary("androidx-activity-compose").get())
+                add("implementation", libs.findLibrary("androidx-lifecycle-viewmodel-compose").get())
+                add("implementation", libs.findLibrary("androidx-navigation-compose").get())
+            }
+        }
+    }
+}
