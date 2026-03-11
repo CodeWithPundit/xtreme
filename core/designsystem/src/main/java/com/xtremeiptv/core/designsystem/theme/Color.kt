b4eb8cdfa71e2377156d6c3594814bf1208a0407
Code With Pundit <codewithpundit@gmail.com>
2026-03-11 12:13:30 -0400

    Create Color.kt

---
+++ core/designsystem/src/main/java/com/xtremeiptv/core/designsystem/theme/Color.kt
@@ -0,0 +1,30 @@
+package com.xtremeiptv.core.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Primary Colors
+val DeepAbyss = Color(0xFF0A0E12)
+val CursedTeal = Color(0xFF005F63)
+val SunkenTimber = Color(0xFF1C2526)
+val HoardersGold = Color(0xFFC9A050)
+val EmeraldGlow = Color(0xFF00FF85)
+val BoneWhite = Color(0xFFE8E2D9)
+
+// Secondary Colors
+val AbyssLight = Color(0xFF1A1E22)
+val TealLight = Color(0xFF1A6F73)
+val TimberLight = Color(0xFF2C3536)
+val GoldLight = Color(0xFFD9B060)
+val GlowLight = Color(0xFF20FF95)
+val WhiteBone = Color(0xFFF8F2E9)
+
+// Status Colors
+val Success = Color(0xFF4CAF50)
+val Error = Color(0xFFF44336)
+val Warning = Color(0xFFFF9800)
+val Info = Color(0xFF2196F3)
+
+// Overlay Colors
+val Scrim = Color(0x99000000)
+val GlassLight = Color(0x33FFFFFF)
+val GlassDark = Color(0x33000000)
