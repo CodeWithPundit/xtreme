b0481d4e7d9966c82a7c385384e3996b45d0363f
Code With Pundit <codewithpundit@gmail.com>
2026-03-11 12:03:01 -0400

    Create XtreamCodesApi.kt

---
+++ core/network/src/main/java/com/xtremeiptv/core/network/api/XtreamCodesApi.kt
@@ -0,0 +1,93 @@
+package com.xtremeiptv.core.network.api
+
+import com.xtremeiptv.core.network.response.*
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.http.Url
+
+interface XtreamCodesApi {
+
+    @GET("player_api.php")
+    suspend fun authenticate(
+        @Query("username") username: String,
+        @Query("password") password: String
+    ): XtreamAuthResponse
+
+    @GET("player_api.php")
+    suspend fun getLiveCategories(
+        @Query("username") username: String,
+        @Query("password") password: String,
+        @Query("action") action: String = "get_live_categories"
+    ): List<XtreamCategory>
+
+    @GET("player_api.php")
+    suspend fun getLiveStreams(
+        @Query("username") username: String,
+        @Query("password") password: String,
+        @Query("action") action: String = "get_live_streams",
+        @Query("category_id") categoryId: String? = null
+    ): List<XtreamLiveStream>
+
+    @GET("player_api.php")
+    suspend fun getVodCategories(
+        @Query("username") username: String,
+        @Query("password") password: String,
+        @Query("action") action: String = "get_vod_categories"
+    ): List<XtreamCategory>
+
+    @GET("player_api.php")
+    suspend fun getVodStreams(
+        @Query("username") username: String,
+        @Query("password") password: String,
+        @Query("action") action: String = "get_vod_streams",
+        @Query("category_id") categoryId: String? = null
+    ): List<XtreamVodStream>
+
+    @GET("player_api.php")
+    suspend fun getSeriesCategories(
+        @Query("username") username: String,
+        @Query("password") password: String,
+        @Query("action") action: String = "get_series_categories"
+    ): List<XtreamCategory>
+
+    @GET("player_api.php")
+    suspend fun getSeries(
+        @Query("username") username: String,
+        @Query("password") password: String,
+        @Query("action") action: String = "get_series",
+        @Query("category_id") categoryId: String? = null
+    ): List<XtreamSeries>
+
+    @GET("player_api.php")
+    suspend fun getSeriesInfo(
+        @Query("username") username: String,
+        @Query("password") password: String,
+        @Query("action") action: String = "get_series_info",
+        @Query("series_id") seriesId: String
+    ): XtreamSeriesInfo
+
+    @GET("player_api.php")
+    suspend fun getVodInfo(
+        @Query("username") username: String,
+        @Query("password") password: String,
+        @Query("action") action: String = "get_vod_info",
+        @Query("vod_id") vodId: String
+    ): XtreamVodInfo
+
+    @GET("player_api.php")
+    suspend fun getShortEpg(
+        @Query("username") username: String,
+        @Query("password") password: String,
+        @Query("action") action: String = "get_short_epg",
+        @Query("stream_id") streamId: String,
+        @Query("limit") limit: Int = 7
+    ): List<XtreamEpgProgram>
+
+    @GET("player_api.php")
+    suspend fun getFullEpg(
+        @Query("username") username: String,
+        @Query("password") password: String,
+        @Query("action") action: String = "get_simple_data_table",
+        @Query("stream_id") streamId: String
+    ): List<XtreamEpgProgram>
+}
